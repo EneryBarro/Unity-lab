99fa356ffd09608d0d513377766f201ae0edd51c
Bohdan Dienin <deninbog1406@gmail.com>
2023-05-21 11:44:27 +0300

    1

---
+++ My project (1)/Library/Bee/artifacts/Android/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,173 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void UIManager::StartGame()
+extern void UIManager_StartGame_mBF354B11E3D121DE0BC0F2DF89F2F5B554462558 (void);
+// 0x00000002 System.Void UIManager::QuitGame()
+extern void UIManager_QuitGame_mC83E00BA927C6D7535F8F91D5851A6815AB5FAD4 (void);
+// 0x00000003 System.Void UIManager::.ctor()
+extern void UIManager__ctor_mC9DC2B8984E76F424E73C1860AD4BD3DEBF6573F (void);
+// 0x00000004 System.Void SpawnEffect::Start()
+extern void SpawnEffect_Start_m07241114876D469E2C6C93AA6E2A33D9D7ED6776 (void);
+// 0x00000005 System.Void SpawnEffect::Update()
+extern void SpawnEffect_Update_mAE49964994B77657A3E847FD62F48C5617817F9F (void);
+// 0x00000006 System.Void SpawnEffect::.ctor()
+extern void SpawnEffect__ctor_m8E4F61228DBAA92E0DBFD6CD8C0AEAB0C072C2FB (void);
+// 0x00000007 System.Void RampAsset::.ctor()
+extern void RampAsset__ctor_m7CB8E98ADF762E7343D9A8AEC915FEA693ED9678 (void);
+// 0x00000008 System.Void AdjustTimeScale::Start()
+extern void AdjustTimeScale_Start_m4A38ED5A991235FDD56B6F7444AC8677B55DC578 (void);
+// 0x00000009 System.Void AdjustTimeScale::Update()
+extern void AdjustTimeScale_Update_m35850BFE55F334D789D2273391B993112BC1F2EE (void);
+// 0x0000000A System.Void AdjustTimeScale::OnApplicationQuit()
+extern void AdjustTimeScale_OnApplicationQuit_mBF83E944940AE95B2542819682EBEAA7580EFDF1 (void);
+// 0x0000000B System.Void AdjustTimeScale::.ctor()
+extern void AdjustTimeScale__ctor_m0D9D7BCA4710EF1B661F7648C598020E3483DCC6 (void);
+// 0x0000000C System.Void ProximityActivate::Start()
+extern void ProximityActivate_Start_mBA100FA3ED59208F73FBE82E9584F9B1DE0450BB (void);
+// 0x0000000D System.Boolean ProximityActivate::IsTargetNear()
+extern void ProximityActivate_IsTargetNear_m826C0D892D66C8B9229003E9E7A7BFC1DAC8195A (void);
+// 0x0000000E System.Void ProximityActivate::Update()
+extern void ProximityActivate_Update_m4A9769EE498773365306E9DCFFEFAFE0058A83DF (void);
+// 0x0000000F System.Void ProximityActivate::.ctor()
+extern void ProximityActivate__ctor_m47AA717E38E4A36C84CDFA7EFF72192658155DB0 (void);
+// 0x00000010 System.Void SimpleCharacterMotor::Awake()
+extern void SimpleCharacterMotor_Awake_m347826CC4184FB98988CC63028CFA1736D438135 (void);
+// 0x00000011 System.Void SimpleCharacterMotor::Update()
+extern void SimpleCharacterMotor_Update_mA5966DE9D18E3A51D7E547A1DB3D6D668CA2199B (void);
+// 0x00000012 System.Void SimpleCharacterMotor::UpdateLookRotation()
+extern void SimpleCharacterMotor_UpdateLookRotation_m7A7D395E0D03FFDAF115286506F06BCED9C33E10 (void);
+// 0x00000013 System.Void SimpleCharacterMotor::UpdateTranslation()
+extern void SimpleCharacterMotor_UpdateTranslation_m73972A962820E750E7A5DA7AEF8BE1F8C512C6B1 (void);
+// 0x00000014 System.Void SimpleCharacterMotor::.ctor()
+extern void SimpleCharacterMotor__ctor_mDF39162F0CD433D75D7AF488911B5865F0620217 (void);
+// 0x00000015 System.Void Readme::.ctor()
+extern void Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6 (void);
+// 0x00000016 System.Void Readme/Section::.ctor()
+extern void Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B (void);
+// 0x00000017 System.String VuforiaLicense::GetLicenseKey()
+extern void VuforiaLicense_GetLicenseKey_m796D51E5EF1C53F3D62BA4EA8806AC1FB1A5E3C4 (void);
+// 0x00000018 System.Void VuforiaLicense::.ctor()
+extern void VuforiaLicense__ctor_m094B92B3323DFFD79ADB696474D5D158C95E291B (void);
+// 0x00000019 System.Void UnityTemplateProjects.SimpleCameraController::OnEnable()
+extern void SimpleCameraController_OnEnable_mD5E060BED4E0C5C0D40AA39C0DA7AE44CAAB217D (void);
+// 0x0000001A UnityEngine.Vector3 UnityTemplateProjects.SimpleCameraController::GetInputTranslationDirection()
+extern void SimpleCameraController_GetInputTranslationDirection_m62C42A7966EF22CB95BB847225D2BE2441A3CE0C (void);
+// 0x0000001B System.Void UnityTemplateProjects.SimpleCameraController::Update()
+extern void SimpleCameraController_Update_mDB9A4DA6CC1C82A704D3575DC196F44F17E5E0B5 (void);
+// 0x0000001C System.Void UnityTemplateProjects.SimpleCameraController::.ctor()
+extern void SimpleCameraController__ctor_m224B705F449A3DA440346B08A6237479A91D7136 (void);
+// 0x0000001D System.Void UnityTemplateProjects.SimpleCameraController/CameraState::SetFromTransform(UnityEngine.Transform)
+extern void CameraState_SetFromTransform_m3A7302F4586F37F7C78EDA2F0C476BAA22672360 (void);
+// 0x0000001E System.Void UnityTemplateProjects.SimpleCameraController/CameraState::Translate(UnityEngine.Vector3)
+extern void CameraState_Translate_m561F6D92E99F13444FBAC8D5B1A40762A26219F9 (void);
+// 0x0000001F System.Void UnityTemplateProjects.SimpleCameraController/CameraState::LerpTowards(UnityTemplateProjects.SimpleCameraController/CameraState,System.Single,System.Single)
+extern void CameraState_LerpTowards_mFDC0E1CA19B6791CDB6EE8AC262944DC1143FCF9 (void);
+// 0x00000020 System.Void UnityTemplateProjects.SimpleCameraController/CameraState::UpdateTransform(UnityEngine.Transform)
+extern void CameraState_UpdateTransform_m03320D760E52E809272776B58C7E741B2EE1D73E (void);
+// 0x00000021 System.Void UnityTemplateProjects.SimpleCameraController/CameraState::.ctor()
+extern void CameraState__ctor_m33568E59E39C953BDE6CD28D35A34BE52AE25504 (void);
+static Il2CppMethodPointer s_methodPointers[33] = 
+{
+	UIManager_StartGame_mBF354B11E3D121DE0BC0F2DF89F2F5B554462558,
+	UIManager_QuitGame_mC83E00BA927C6D7535F8F91D5851A6815AB5FAD4,
+	UIManager__ctor_mC9DC2B8984E76F424E73C1860AD4BD3DEBF6573F,
+	SpawnEffect_Start_m07241114876D469E2C6C93AA6E2A33D9D7ED6776,
+	SpawnEffect_Update_mAE49964994B77657A3E847FD62F48C5617817F9F,
+	SpawnEffect__ctor_m8E4F61228DBAA92E0DBFD6CD8C0AEAB0C072C2FB,
+	RampAsset__ctor_m7CB8E98ADF762E7343D9A8AEC915FEA693ED9678,
+	AdjustTimeScale_Start_m4A38ED5A991235FDD56B6F7444AC8677B55DC578,
+	AdjustTimeScale_Update_m35850BFE55F334D789D2273391B993112BC1F2EE,
+	AdjustTimeScale_OnApplicationQuit_mBF83E944940AE95B2542819682EBEAA7580EFDF1,
+	AdjustTimeScale__ctor_m0D9D7BCA4710EF1B661F7648C598020E3483DCC6,
+	ProximityActivate_Start_mBA100FA3ED59208F73FBE82E9584F9B1DE0450BB,
+	ProximityActivate_IsTargetNear_m826C0D892D66C8B9229003E9E7A7BFC1DAC8195A,
+	ProximityActivate_Update_m4A9769EE498773365306E9DCFFEFAFE0058A83DF,
+	ProximityActivate__ctor_m47AA717E38E4A36C84CDFA7EFF72192658155DB0,
+	SimpleCharacterMotor_Awake_m347826CC4184FB98988CC63028CFA1736D438135,
+	SimpleCharacterMotor_Update_mA5966DE9D18E3A51D7E547A1DB3D6D668CA2199B,
+	SimpleCharacterMotor_UpdateLookRotation_m7A7D395E0D03FFDAF115286506F06BCED9C33E10,
+	SimpleCharacterMotor_UpdateTranslation_m73972A962820E750E7A5DA7AEF8BE1F8C512C6B1,
+	SimpleCharacterMotor__ctor_mDF39162F0CD433D75D7AF488911B5865F0620217,
+	Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6,
+	Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B,
+	VuforiaLicense_GetLicenseKey_m796D51E5EF1C53F3D62BA4EA8806AC1FB1A5E3C4,
+	VuforiaLicense__ctor_m094B92B3323DFFD79ADB696474D5D158C95E291B,
+	SimpleCameraController_OnEnable_mD5E060BED4E0C5C0D40AA39C0DA7AE44CAAB217D,
+	SimpleCameraController_GetInputTranslationDirection_m62C42A7966EF22CB95BB847225D2BE2441A3CE0C,
+	SimpleCameraController_Update_mDB9A4DA6CC1C82A704D3575DC196F44F17E5E0B5,
+	SimpleCameraController__ctor_m224B705F449A3DA440346B08A6237479A91D7136,
+	CameraState_SetFromTransform_m3A7302F4586F37F7C78EDA2F0C476BAA22672360,
+	CameraState_Translate_m561F6D92E99F13444FBAC8D5B1A40762A26219F9,
+	CameraState_LerpTowards_mFDC0E1CA19B6791CDB6EE8AC262944DC1143FCF9,
+	CameraState_UpdateTransform_m03320D760E52E809272776B58C7E741B2EE1D73E,
+	CameraState__ctor_m33568E59E39C953BDE6CD28D35A34BE52AE25504,
+};
+static const int32_t s_InvokerIndices[33] = 
+{
+	5102,
+	5102,
+	5102,
+	5102,
+	5102,
+	5102,
+	5102,
+	5102,
+	5102,
+	5102,
+	5102,
+	5102,
+	4925,
+	5102,
+	5102,
+	5102,
+	5102,
+	5102,
+	5102,
+	5102,
+	5102,
+	5102,
+	7578,
+	5102,
+	5102,
+	5093,
+	5102,
+	5102,
+	4058,
+	4142,
+	1263,
+	4058,
+	5102,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	33,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
